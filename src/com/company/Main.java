b90f467ecdb665533e8b06bc493b7cb48a09264b
bbarnefiher <bb222@pvsd.org>
2018-11-26 11:22:13 -0500

    end of class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int ranNum = -1;
+        int[] uniqueNumbers = new int[101];
+        int count = 0;
+        int timesLoopRuns = 0;
+        int attemptCount = 0;
+        Scanner stringReader = new Scanner(System.in);
+        System.out.println("How many times");
+        String userInput = stringReader.nextLine();
+        while (count != 100){
+            timesLoopRuns++;
+        for (int i = 0; i < 10; i++) { //generates random number and puts it into array
+            attemptCount++;
+            ranNum = (int) (Math.random() * 100) + 1;
+            if (ranNum != uniqueNumbers[ranNum]) {
+                uniqueNumbers[ranNum] = ranNum;
+                count++;
+            }
+            if (count == 100){
+                break;
+            }
+        }
+        }
+        for (int i = 1; i < uniqueNumbers.length; i++){
+            System.out.println(uniqueNumbers[i]);
+        }
+        System.out.println("number of loop iterations: " + timesLoopRuns);
+        System.out.println("total attempts: " + attemptCount);
+
+
+
+
     }
 }
---
+++ src/com/company/uniqueNum.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by bb222 on 11/26/18.
+ */
+public class uniqueNum {
+    String[] uniqueNumbers = new String[100];
+    public uniqueNum(int a){
+        //uniqueNumbers.
+    }
+}
