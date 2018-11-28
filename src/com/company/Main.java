35d548c1b792804c473ae2a0a8f9f501165ec884
bbarnefiher <bb222@pvsd.org>
2018-11-28 11:20:27 -0500

    end of class 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,37 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         int ranNum = -1;
+        int numAverageTimesRun;
         int[] uniqueNumbers = new int[101];
         int count = 0;
         int timesLoopRuns = 0;
         int attemptCount = 0;
-        Scanner stringReader = new Scanner(System.in);
-        System.out.println("How many times");
-        String userInput = stringReader.nextLine();
-        while (count != 100){
-            timesLoopRuns++;
-        for (int i = 0; i < 10; i++) { //generates random number and puts it into array
-            attemptCount++;
-            ranNum = (int) (Math.random() * 100) + 1;
-            if (ranNum != uniqueNumbers[ranNum]) {
-                uniqueNumbers[ranNum] = ranNum;
-                count++;
+
+        int timesRun = 100;
+        for (int i = 0; i < timesRun; i++) {//makes entire loop run x many times
+            while (count != 100) {
+                timesLoopRuns++;
+                for (int k = 0; k < 10; k++) { //generates random number and puts it into array
+                    attemptCount++;
+                    ranNum = (int) (Math.random() * 100) + 1;
+                    if (ranNum != uniqueNumbers[ranNum]) {
+                        uniqueNumbers[ranNum] = ranNum;
+                        count++;
+                    }
+                    if (count == 100) {
+                        break;
+                    }
+                }
+                
             }
-            if (count == 100){
-                break;
-            }
+
         }
-        }
-        for (int i = 1; i < uniqueNumbers.length; i++){
-            System.out.println(uniqueNumbers[i]);
+        for (int j = 1; j < uniqueNumbers.length; j++){
+            System.out.println(uniqueNumbers[j]);
         }
         System.out.println("number of loop iterations: " + timesLoopRuns);
         System.out.println("total attempts: " + attemptCount);
